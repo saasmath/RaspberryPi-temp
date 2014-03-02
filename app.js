9af7c3cd409fd9c39e5f296b864530af1c27f32f
Gary Anderson <git@garyvictoranderson.com>
2014-03-02 21:19:09 +0000

    now reads processor temp; updated install version of socket.io

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(req,res){
         res.writeHead(200,{'Content-Type':'text/html'});
         res.end(data, 'utf-8');
     });
-}).listen(3000);
+}).listen(3001);
 
 console.log('Server is running');
 
@@ -16,9 +16,9 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', function(socket){
     console.log('User connected');
     setInterval(function(){
-    fs.readFile('/sys/bus/w1/devices/28-00000451e0e3/w1_slave', function(error, data){
-        console.log(data.toString().substring(69)/1000);
-        socket.emit('temperature',{number: data.toString().substring(69)/1000});
+    fs.readFile('/sys/class/thermal/thermal_zone0/temp', function(error, data){
+        //console.log(data.toString()/1000);
+        socket.emit('temperature',{number: data.toString()/1000});
     });
     }, 1000);
 });
